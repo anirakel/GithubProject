21e1418137db5638e56942e50a2bc62b398a0a6d
anirakel <anirakel1997@gmail.com>
2023-09-15 21:15:27 +0400

    Add existing project files to Git

--- src/test/java/HomeworkforLesson22.java
+++ src/test/java/HomeworkforLesson22.java
@@ -1,36 +1,26 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
-import static java.time.Duration.*;
 
-public class GithubLoginTest {
-    @Test
-    public void ChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://github.com/login");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        driver.quit();
-    }
-
+public class HomeworkforLesson22 {
+    private WebDriver driver;
 
     @Test
+    // GitHub login Test
+
     public void loginTest() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://github.com/login");
         WebElement emailElement = driver.findElement(By.id("login_field"));
         emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
@@ -38,72 +28,66 @@ public class GithubLoginTest {
         password.sendKeys("Jermuk.1");
         WebElement loginElement = driver.findElement(By.name("commit"));
         loginElement.click();
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        By locatorOfAvatarElement = By.cssSelector(".Button-label .avatar.circle");
+        SeleniumActions actions = new SeleniumActions(driver);
+        boolean isLocatorOfAvatarElementDisplayed = actions.isDisplayed(locatorOfAvatarElement, 5);
+        Assert.assertTrue(isLocatorOfAvatarElementDisplayed);
+        driver.quit();
 
     }
 
-
     @Test
-    public void loginTestNegative1() {
+    // This method is validating that user cannot log in with empty password.
+    public void EmptyPassword() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://github.com/login");
         WebElement emailElement = driver.findElement(By.id("login_field"));
         emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
         WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("Jermuk111");
+        password.sendKeys("");
         WebElement loginElement = driver.findElement(By.name("commit"));
         loginElement.click();
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        WebElement errorElement = driver.findElement(By.className("flash flash-full flash-error"));
-
-        System.out.println(errorElement.getText());
-
+        WebElement errorElement = driver.findElement(By.className("js-flash-alert"));
         Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
-
-    }
-
-    @Test
-    public void loginTestNegative2() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://github.com/login");
-        WebElement emailElement = driver.findElement(By.id("login_field"));
-        emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("  ");
-        WebElement loginElement = driver.findElement(By.name("commit"));
-        loginElement.click();
         try {
-            Thread.sleep(10000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-    }
-    //FirefoxDriver
-
-    @Test
-    public void FirefoxDriver() {
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
-        FirefoxDriver driver = new FirefoxDriver();
-        driver.get("https://github.com/login ");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        By locatorOfAvatarElement = By.cssSelector(".Button-label .avatar.circle");
+        SeleniumActions actions = new SeleniumActions(driver);
+        boolean isLocatorOfAvatarElementDisplayed = actions.isDisplayed(locatorOfAvatarElement, 5);
+        Assert.assertTrue(isLocatorOfAvatarElementDisplayed);
         driver.quit();
     }
+
+    // This method is validating that user cannot log in with wrong password.
+    @Test
+    public void WrongPassword() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://github.com/login");
+        WebElement emailElement = driver.findElement(By.id("login_field"));
+        emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("wrongpassword");
+        WebElement loginElement = driver.findElement(By.name("commit"));
+        loginElement.click();
+        WebElement errorElement = driver.findElement(By.className("js-flash-alert"));
+        Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        By locatorOfAvatarElement = By.cssSelector(".Button-label .avatar.circle");
+        SeleniumActions actions = new SeleniumActions(driver);
+        boolean isLocatorOfAvatarElementDisplayed = actions.isDisplayed(locatorOfAvatarElement, 5);
+        Assert.assertTrue(isLocatorOfAvatarElementDisplayed);
+        driver.quit();
+    }
+    //GitHub login with Firefox
 
     @Test
     public void loginTestFirefox() {
@@ -122,19 +106,23 @@ public class GithubLoginTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+        By locatorOfAvatarElement = By.cssSelector(".Button-label .avatar.circle");
+        SeleniumActions actions = new SeleniumActions(driver);
+        boolean isLocatorOfAvatarElementDisplayed = actions.isDisplayed(locatorOfAvatarElement, 5);
+        Assert.assertTrue(isLocatorOfAvatarElementDisplayed);
 
     }
-
+    //This method is validating that user cannot log in with empty password.
 
     @Test
-    public void loginTestNegative1Firefox() {
+    public void loginTestEmptyPasswordFirefox() {
         System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
         FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://github.com/login");
         WebElement emailElement = driver.findElement(By.id("login_field"));
         emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
         WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("Jermuk111");
+        password.sendKeys("");
         WebElement loginElement = driver.findElement(By.name("commit"));
         loginElement.click();
 
@@ -143,44 +131,31 @@ public class GithubLoginTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        WebElement errorElement = driver.findElement(By.className("flash flash-full flash-error"));//xndiry aystexic e
-        System.out.println(errorElement.getText());
-
+        WebElement errorElement = driver.findElement(By.className("js-flash-alert"));//xndiry aystexic e
         Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        By locatorOfAvatarElement = By.cssSelector(".Button-label .avatar.circle");
+        SeleniumActions actions = new SeleniumActions(driver);
+        boolean isLocatorOfAvatarElementDisplayed = actions.isDisplayed(locatorOfAvatarElement, 5);
+        Assert.assertTrue(isLocatorOfAvatarElementDisplayed);
 
     }
 
+    //This method is validating that user cannot log in with wrong password.
     @Test
-    public void loginTestNegative2Firefox() {
-
+    public void loginTestWrongPassword() {
         System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
         FirefoxDriver driver = new FirefoxDriver();
         driver.get("https://github.com/login");
         WebElement emailElement = driver.findElement(By.id("login_field"));
         emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
         WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("Jermuk.1");
+        password.sendKeys("wrongpassword");
         WebElement loginElement = driver.findElement(By.name("commit"));
-        WebElement firstResult = new WebDriverWait(driver, (10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//a/h3")));
-        loginElement.click();
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    @Test
-    public void login() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://github.com/login");
-        WebElement emailElement = driver.findElement(By.id("login_field"));
-        emailElement.sendKeys("ani-arakelyan-1997@mail.ru");
-        WebElement password = driver.findElement(By.id("password"));
-        password.sendKeys("Jermuk111");
-        WebElement loginElement = driver.findElement(By.name("avatar avatar-user"));
         loginElement.click();
 
         try {
@@ -188,12 +163,21 @@ public class GithubLoginTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
-//        WebElement errorElement = driver.findElement(By.className("//span[@class=\"Button-label\"]/img[@class=\"avatar circle\"]");
-//        System.out.println(errorElement.getText());
-//
-//        Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
-
-
+        WebElement errorElement = driver.findElement(By.className("js-flash-alert"));//xndiry aystexic e
+        Assert.assertEquals(errorElement.getText(), "Incorrect username or password.");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        By locatorOfAvatarElement = By.cssSelector(".Button-label .avatar.circle");
+        SeleniumActions actions = new SeleniumActions(driver);
+        boolean isLocatorOfAvatarElementDisplayed = actions.isDisplayed(locatorOfAvatarElement, 5);
+        Assert.assertTrue(isLocatorOfAvatarElementDisplayed);
     }
 }
+
+
+
+
+
